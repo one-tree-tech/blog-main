13f5acfe1b8b5bb3dc7c7204f0a66940a18d1864
Kirk Wood <1tree@users.noreply.github.com>
2026-01-18 22:23:36 -0600

    general tweaks

--- apps/blog/src/app/posts/_template/blog/blog.ts
+++ apps/blog/src/app/posts/_template/blog/blog.ts
@@ -1,14 +1,17 @@
-import { Component, signal } from '@angular/core';
+import { Component, model, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Code } from '../code/code';
 import { Demo } from '../demo/demo';
 import { Explanation } from '../explanation/explanation';
 
 @Component({
   selector: 'app-blog-rxjs-timers',
-  imports: [Demo, Explanation],
+  imports: [Demo, Explanation, FormsModule, Code],
   templateUrl: './blog.html',
   styleUrl: './blog.css',
 })
 export class Blog {
+  readonly display = model<string>('explain');
   readonly showDemo = signal(true);
 
   reset() {
